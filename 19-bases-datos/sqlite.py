dab66687e7b307f907616fdc460d55562c59c35d
Fabian Taranto <fataranto@gmail.com>
2024-12-08 13:58:57 +0100

    bbdd sqlite all

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -18,6 +18,36 @@ cursor.execute("CREATE TABLE IF NOT EXISTS productos("+
 # guardarr cambios
 conexion.commit()
 
+# insertar datos
+"""
+cursor.execute("INSERT INTO productos VALUES (null, 'producto uno', 'descripción de mi producto', 250)")
+conexion.commit()
+"""
+
+# borrar registros
+cursor.execute("DELETE FROM productos")
+conexion.commit()
+
+# insertar muchos registros de golpe
+productos = [
+    ("Ordenador portatil", "bueno y bonito", 700),
+    ("Tablet", "plana", 400),
+    ("telefono movil", "sin cables", 1700),
+    ("wifi", "cableado", 100)
+]
+
+cursor.executemany("INSERT INTO productos VALUES (NULL, ?, ?, ?)", productos)
+conexion.commit()
+
+# listar datos
+cursor.execute("SELECT * FROM productos;")
+productos = cursor.fetchall()
+
+for producto in productos:
+    print(producto)
+    print("---------------------------")
+
+print(cursor)
 
 # cerrar conexión
 conexion.close()
